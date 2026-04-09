ebc26bc131dbaa2f76c561ae428576c87a181192
Duvan <jhonnier.guevara00@usc.edu.co>
2026-04-09 11:03:41 -0500

    elimine todo enter the commit message for your changes. Lines starting

--- src/middlewares/error.middleware.js
+++
@@ -1,7 +0,0 @@
-exports.manejarErrores = (err, req, res, next) => {
-    console.error("Error detectado:", err.message);
-
-    res.status(500).json({
-        error: "Error interno del servidor"
-    });
-};
