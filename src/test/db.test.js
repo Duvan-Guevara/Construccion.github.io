283933ddf25845a2a6f63dca3ae61ad896ceb254
Duvan <jhonnier.guevara00@usc.edu.co>
2026-05-17 12:33:46 -0500

    elimine todo

--- src/test/db.test.js
+++
@@ -1,9 +0,0 @@
-const pool = require("../config/db");
-
-pool.query("SELECT NOW()")
-  .then(res => {
-    console.log("✅ PostgreSQL conectado:", res.rows[0]);
-  })
-  .catch(err => {
-    console.error("❌ Error DB:", err);
-  });
